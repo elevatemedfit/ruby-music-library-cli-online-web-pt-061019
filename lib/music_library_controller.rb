a822904d827355067a778aa5e08026460b437079
Chris Junker <elevatemedfit@gmail.com>
2019-07-28 18:31:22 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -81,8 +81,8 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     song_number = gets.strip.to_i
-    if song_number.to_i.between?(1, Song.all.length)
-      song = list_songs[song_number.to_i]
+    if song_number.between?(1, Song.all.length)
+      song = list_songs[song_number]
       puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
